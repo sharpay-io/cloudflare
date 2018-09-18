8893c02d16c973266a6469392d1435445a46dbf7
Valery Yushchenko <valery.yushchenko@gmail.com>
2018-09-18 11:08:33 +0300

    data-mini option

--- source/app.js
+++ source/app.js
@@ -1,8 +1,12 @@
 (function () {
+
+	/*global INSTALL_OPTIONS*/
+
 	window.addEventListener("load", function () {
 		var options = INSTALL_OPTIONS;
 		var code = (options.code || '').trim();
 		if (!code) {
+			// eslint-disable-next-line no-console
 			console.log('Sharpay: Disabled. Register your site on https://app.sharpay.io/');
 			return;
 		}
@@ -12,25 +16,30 @@
 		vendorDiv.setAttribute('data-sharpay', code);
 
 		if( options.position == 'left' ) {
-		   vendorDiv.setAttribute('data-left', 'true');
+			vendorDiv.setAttribute('data-left', 'true');
 		} else {
-		  vendorDiv.setAttribute('data-right', 'true');
+			vendorDiv.setAttribute('data-right', 'true');
 		}
 
 		if( options.color == 'dark' ) {
-		   vendorDiv.setAttribute('data-dark', 'true');
+			vendorDiv.setAttribute('data-dark', 'true');
 		} else {
-		  vendorDiv.setAttribute('data-light', 'true');
+			vendorDiv.setAttribute('data-light', 'true');
 		}
 
 		if( options.counter == 'separate' ) {
-		   vendorDiv.setAttribute('data-sharecount', 'true');
+			vendorDiv.setAttribute('data-sharecount', 'true');
 		} else if( options.counter == 'all' ) {
-		  vendorDiv.setAttribute('data-sharecount', 'true');
-		  vendorDiv.setAttribute('data-sharecount-mode', 'site');
+			vendorDiv.setAttribute('data-sharecount', 'true');
+			vendorDiv.setAttribute('data-sharecount-mode', 'site');
 		}
+
+		if ( options.mini ) {
+			vendorDiv.setAttribute('data-mini', 'true');
+		}
+
 		document.body.appendChild(vendorDiv);
-		
+
 		var vendorScript = document.createElement('script');
 		vendorScript.src = 'https://app.sharpay.io/api/script.js';
 		document.body.appendChild(vendorScript);
